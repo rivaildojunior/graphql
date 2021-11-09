efef44158388e9bbf8f6271a662e888faaffc476
Rivaildo Junior <rivaildojunior@yahoo.com.br>
2021-11-09 20:16:12 -0300

    Ajuste

--- src/main/java/com/example/graphqldemo/model/dto/ProdutoInput.java
+++ src/main/java/com/example/graphqldemo/model/dto/ProdutoInput.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Id;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProdutoInput {  @Id
+public class ProdutoInput {
 
     private Long id;
     private String nome;
